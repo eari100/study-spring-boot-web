99a0ea3ad935c925faf4b5190e3c52ee75f06359
Jaewook <eari100@naver.com>
2023-01-24 18:22:08 +0900

    Initial commit

---
+++ src/main/java/com/example/studyspringbootweb/StudySpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyspringbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBootWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringBootWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyspringbootweb/StudySpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyspringbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringBootWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
