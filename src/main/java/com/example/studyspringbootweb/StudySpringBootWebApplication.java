c52d745ed0767c65f5dca5e6b8e5347a255aa544
Jaewook Choi <eari100@naver.com>
2023-01-28 23:20:55 +0900

    feat: 스프링 빌드 시 더미 데이터 추가

--- src/main/java/com/example/studyspringbootweb/StudySpringBootWebApplication.java
+++ src/main/java/com/example/studyspringbootweb/StudySpringBootWebApplication.java
@@ -1,7 +1,17 @@
 package com.example.studyspringbootweb;
 
+import com.example.studyspringbootweb.domain.Board;
+import com.example.studyspringbootweb.domain.Users;
+import com.example.studyspringbootweb.domain.enums.BoardType;
+import com.example.studyspringbootweb.repository.BoardRepository;
+import com.example.studyspringbootweb.repository.UsersRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class StudySpringBootWebApplication {
@@ -10,4 +20,28 @@ public class StudySpringBootWebApplication {
         SpringApplication.run(StudySpringBootWebApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner runner(UsersRepository usersRepository, BoardRepository boardRepository) {
+        return args -> {
+            Users users = usersRepository.save(
+                    Users.builder()
+                            .name("havi")
+                            .password("test")
+                            .email("havi@gmail.com")
+                            .createdDate(LocalDateTime.now())
+                            .build());
+
+                    IntStream.rangeClosed(1, 200).forEach(index ->
+                            boardRepository.save(
+                                    Board.builder()
+                                            .title("게시물"+index)
+                                            .subTitle("순서"+index)
+                                            .content("콘텐츠")
+                                            .boardType(BoardType.free)
+                                            .createdDate(LocalDateTime.now())
+                                            .updatedDate(LocalDateTime.now())
+                                            .users(users)
+                                            .build()));
+        };
+    }
 }
