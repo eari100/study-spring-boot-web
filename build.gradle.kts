95eb9dbbad67bfa0fad196554a6a0570e6383e24
Jaewook Choi <eari100@naver.com>
2023-03-12 02:49:48 +0900

    update: spring boot 버전 변경 2.7.8 -> 2.7.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("org.springframework.boot") version "2.7.8"
-    id("io.spring.dependency-management") version "1.1.0"
+    id("org.springframework.boot") version "2.7.9"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
 }
 
 group = "com.example"
